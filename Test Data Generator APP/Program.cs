bdb63b89c93dd91fc83e3414f40556f38b73fbf3
janne-remes <janneremes@outlook.com>
2020-04-14 10:59:37 +0300

    Add project files.

---
+++ Test Data Generator APP/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using TestDataGeneratorLibrary;
+
+namespace Test_Data_Generator_APP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person dudeOne = new Person("Pekka", "Paavola", 54);
+
+            Console.WriteLine(dudeOne.getData());
+        }
+    }
+}
---
+++ TestDataGeneratorLibrary/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestDataGeneratorLibrary
+{
+    public class Person
+    {
+        public string firstName { get; set; }
+
+        public string lastName { get; set; }
+
+        public int age { get; set; }
+
+        public Person()
+        {
+
+        }
+
+        public Person(string firstName, string lastName, int age)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+        }
+
+        public string getData()
+        {
+            return $"{firstName} {lastName}, {age} vuotta";
+        }
+    }
+}
